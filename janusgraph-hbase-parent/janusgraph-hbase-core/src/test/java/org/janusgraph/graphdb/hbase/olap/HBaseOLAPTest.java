f0b50023d60ea38d64be34a86cdf436529c1ef14
Jason Plurad <pluradj@us.ibm.com>
2018-09-16 13:46:53 -0400

    test travis fix 14
    Signed-off-by: Jason Plurad <pluradj@us.ibm.com>

--- janusgraph-hbase-parent/janusgraph-hbase-core/src/test/java/org/janusgraph/graphdb/hbase/olap/HBaseOLAPTest.java
+++ janusgraph-hbase-parent/janusgraph-hbase-core/src/test/java/org/janusgraph/graphdb/hbase/olap/HBaseOLAPTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.janusgraph.graphdb.hbase;
+package org.janusgraph.graphdb.hbase.olap;
 
 import org.janusgraph.HBaseStorageSetup;
 import org.janusgraph.diskstorage.configuration.WriteConfiguration;
